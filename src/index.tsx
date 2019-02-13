fe8bae04c263762bbdf6736962efe26e53fab29d
Manuel Riezebosch <mriezebosch@gmail.com>
2019-02-13 18:50:27 +0100

    fix(web): use plain javascript and anchor for selecting element
    because router paths do not work well with static hosting

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.css';
-import App from './Repositories';
 import { initializeIcons } from '@uifabric/icons';
-
-const router = require('react-router-dom');
 import Repositories from './Repositories';
 import Releases from './Releases';
 
 initializeIcons();
 
-const route = (<router.BrowserRouter>
-    <div>
-        <router.Route path="/repositories" component={Repositories} />
-        <router.Route path="/releases" component={Releases} />
-    </div>
-</router.BrowserRouter>);
-ReactDOM.render(route, document.getElementById('root'));
+const route = window.location.hash.substr(1);
+let element: JSX.Element = (<span />)
+if (route === 'repositories') {
+    element = (<Repositories />);
+} else if (route === 'releases') {
+    element = (<Releases />);
+}
+
+ReactDOM.render(element, document.getElementById('root'));
