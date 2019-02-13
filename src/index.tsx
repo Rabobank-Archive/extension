e699490291bed434e258fc9d2b622395d4d709c3
Manuel Riezebosch <mriezebosch@gmail.com>
2019-02-13 19:32:34 +0100

    fix(web): resolve web extension sdk to include in build output

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,34 @@ import './index.css';
 import { initializeIcons } from '@uifabric/icons';
 import Repositories from './Repositories';
 import Releases from './Releases';
+const VSS = require('vss-web-extension-sdk/lib/VSS.SDK');
 
 initializeIcons();
 
-const route = window.location.hash.substr(1);
-let element: JSX.Element = (<span />)
-if (route === 'repositories') {
-    element = (<Repositories />);
-} else if (route === 'releases') {
-    element = (<Releases />);
+// only do VSS stuff when running as part of the extension
+if (typeof VSS != 'undefined') {
+    VSS.init({
+        setupModuleLoader: true,
+        explicitNotifyLoaded: false,
+        usePlatformScripts: true,
+        usePlatformStyles: true
+    });
+}
+
+// Not using react router because the paths are incompatible
+// with static hosting on Azure Devops extensions therefore
+// using index.html#report as identifier.
+let element: JSX.Element;
+const report = window.location.hash.substr(1);
+switch (report) {
+    case 'repositories':
+        element = (<Repositories />);
+        break;
+    case 'releases':
+        element = (<Releases />);
+        break;
+    default:
+        element = (<span className="error">No report specified.</span>)
 }
 
 ReactDOM.render(element, document.getElementById('root'));
