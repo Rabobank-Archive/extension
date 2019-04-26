9f5f6ff7fb84ee21033992c9fc77d49718dfb2f0
Verhaar, CA (Kees) <kees.verhaar@rabobank.nl>
2019-04-26 12:55:40 +0200

    Implemented sorting

--- src/components/RepositoriesMasterDetail.tsx
+++ src/components/RepositoriesMasterDetail.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Card } from "azure-devops-ui/Card";
-import { IObservableValue, ObservableValue } from "azure-devops-ui/Core/Observable";
+import { IObservableValue, ObservableValue, ObservableArray } from "azure-devops-ui/Core/Observable";
 import { Header, TitleSize } from "azure-devops-ui/Header";
 import { IListItemDetails, List, ListItem, ListSelection } from "azure-devops-ui/List";
 import { DetailsPanel, MasterPanel, MasterPanelHeader } from "azure-devops-ui/MasterDetails";
@@ -19,13 +19,14 @@ import { TextField } from "azure-devops-ui/TextField";
 import { ArrayItemProvider } from "azure-devops-ui/Utilities/Provider";
 import { renderString, renderCheckmark } from "./TableRenderers";
 import { IStatusProps, Statuses } from "azure-devops-ui/Status";
+import { sortingBehavior } from "./TableBehaviors";
 
 interface IReportMaster {
     item: string,
-    rules: IReportDetail[]
+    rules: IReportRule[]
 }
 
-interface IReportDetail {
+interface IReportRule {
     description: string,
     status: IStatusProps
 }
@@ -35,7 +36,7 @@ export default class extends React.Component<{ data: { item: string, rules:{ des
         let master: IReportMaster = {
             item: m.item,
             rules: m.rules.map(r => {
-                let rule: IReportDetail = {
+                let rule: IReportRule = {
                     description: r.description,
                     status: r.status ? Statuses.Success : Statuses.Failed
                 }
@@ -122,16 +123,31 @@ export default class extends React.Component<{ data: { item: string, rules:{ des
     }> = props => {
         const { detailItem } = props;
     
-        const columns: ITableColumn<IReportDetail>[] = [
+        const columns: ITableColumn<IReportRule>[] = [
             {
                 id: "description",
                 name: "Description",
                 width: new ObservableValue(450),
-                renderCell: renderString
+                renderCell: renderString,
+                sortProps: {
+                    ariaLabelAscending: "Sorted A to Z",
+                    ariaLabelDescending: "Sorted Z to A"
+                }
             },
-            { id: "status", name: "Status", width: new ObservableValue(75), renderCell: renderCheckmark }
+            { 
+                id: "status", 
+                name: "Status", 
+                width: new ObservableValue(75), 
+                renderCell: renderCheckmark,
+                sortProps: {
+                    ariaLabelAscending: "Sorted A to Z",
+                    ariaLabelDescending: "Sorted Z to A"
+                }
+            }
         ];
     
+        let itemProvider = new ObservableArray<IReportRule>(detailItem.rules);
+
         return (
             <Page>
                 <Header
@@ -144,10 +160,11 @@ export default class extends React.Component<{ data: { item: string, rules:{ des
                         className="bolt-card-no-vertical-padding"
                         contentProps={{ contentPadding: false }}
                     >
-                        <Table<IReportDetail>
+                        <Table<IReportRule>
                             columns={columns}
-                            itemProvider={new ArrayItemProvider<IReportDetail>(detailItem.rules)}
+                            itemProvider={itemProvider}
                             showLines={true}
+                            behaviors={[ sortingBehavior(itemProvider, columns) ]}
                         />
                     </Card>
                 </div>
