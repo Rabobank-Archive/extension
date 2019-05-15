383b920ff931092f8070c17a2479bb9b7322c50c
Ruben Oostinga <roostinga@xebia.com>
2019-05-15 17:39:56 +0200

    Cleanup ReconcileButton.spec

--- src/components/ReconcileButton.spec.tsx
+++ src/components/ReconcileButton.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import fetchMock from 'fetch-mock';
-import { ReconcileButton } from "./ReconcileButton";
+import ReconcileButton from "./ReconcileButton";
 import { fireEvent, render, wait } from "react-testing-library";
 import { DummyAzDoService } from "../services/DummyAzDoService";
 import { CompliancyCheckerService } from "../services/CompliancyCheckerService";
@@ -13,16 +13,15 @@ describe('ReconcileButon', () => {
       compliancyCheckerService: new CompliancyCheckerService(new DummyAzDoService())
     }}/>);
 
-    const reconcileButton = getByText('Reconcile');
-    fireEvent.click(reconcileButton);
+    fireEvent.click(getByText('Reconcile'));
 
-    const reconcileButtons = getAllByText('Reconcile');
-    expect(reconcileButtons).toHaveLength(2);
 
     fetchMock.get('*', 200);
 
     getByText('Confirm reconciliation');
 
+    const reconcileButtons = getAllByText('Reconcile');
+    expect(reconcileButtons).toHaveLength(2);
     const dialogConfirmButton = reconcileButtons[1];
     fireEvent.click(dialogConfirmButton);
 
@@ -31,7 +30,64 @@ describe('ReconcileButon', () => {
     });
   });
 
-  it('should show a loading state while reconciling', async () => {});
-  it('should show an error when reconciling failed', async () => {});
-  it('should dismiss the dialog', async () => {});
+  it('should show a loading state while reconciling', async () => {
+    const { debug, getByText, getAllByText, findByText, container } = render(<ReconcileButton reconcilableItem={{
+      reconcileUrl: '/mock-url',
+      reconcileImpact: ['mock-impact'],
+      compliancyCheckerService: new CompliancyCheckerService(new DummyAzDoService())
+    }}/>);
+
+    fireEvent.click(getByText('Reconcile'));
+
+    const reconcileButtons = getAllByText('Reconcile');
+    expect(reconcileButtons).toHaveLength(2);
+
+    fetchMock.get('*', 200);
+
+    const dialogConfirmButton = reconcileButtons[1];
+    fireEvent.click(dialogConfirmButton);
+
+    wait(() => {
+      getByText('Reconciling...');
+    });
+  });
+
+  it('should show an error when reconciling failed', async () => {
+    const { debug, getByText, getAllByText, findByText, container } = render(<ReconcileButton reconcilableItem={{
+      reconcileUrl: '/mock-url',
+      reconcileImpact: ['mock-impact'],
+      compliancyCheckerService: new CompliancyCheckerService(new DummyAzDoService())
+    }}/>);
+
+    fireEvent.click(getByText('Reconcile'));
+
+    fetchMock.get('*', 500);
+
+    const reconcileButtons = getAllByText('Reconcile');
+    expect(reconcileButtons).toHaveLength(2);
+    const dialogConfirmButton = reconcileButtons[1];
+    fireEvent.click(dialogConfirmButton);
+
+    wait(() => {
+      getByText('Couldn\'t fulfill reconcile request.')
+    })
+  });
+
+  it('should dismiss the dialog', async () => {
+    const { debug, getByText, getAllByText, findByText, container } = render(<ReconcileButton reconcilableItem={{
+      reconcileUrl: '/mock-url',
+      reconcileImpact: ['mock-impact'],
+      compliancyCheckerService: new CompliancyCheckerService(new DummyAzDoService())
+    }}/>);
+
+    fireEvent.click(getByText('Reconcile'));
+
+    getByText('Confirm reconciliation');
+
+    fireEvent.click(getByText('Cancel'));
+
+    wait(() => {
+      expect(findByText('Confirm reconciliation')).toBeNull();
+    });
+  });
 });
