50533b95c68a50463b3f46c1cb9fa4fe8468c788
Verhaar, CA (Kees) <kees.verhaar@rabobank.nl>
2019-05-17 13:49:45 +0200

    Fix formatting and move doFetch into useEffect hook

--- src/components/ReconcileButton.tsx
+++ src/components/ReconcileButton.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import { useEffect, useState } from 'react';
-import { Button } from 'azure-devops-ui/Button';
+import * as React from "react";
+import { useEffect, useState } from "react";
+import { Button } from "azure-devops-ui/Button";
 import { Dialog } from "azure-devops-ui/Dialog";
 import { MessageCard, MessageCardSeverity } from "azure-devops-ui/MessageCard";
 import { SimpleList } from "azure-devops-ui/List";
@@ -16,88 +16,110 @@ interface IReconcileButtonProps {
     };
 }
 
-export const ReconcileButton = ({ reconcilableItem }: IReconcileButtonProps) => {
-        const [hasError, setHasError] = useState<boolean>( false);
-        const [errorText, setErrorText] = useState<string>('');
-        const [isReconciling, setIsReconciling] = useState<boolean>(false);
-        const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+export const ReconcileButton = ({
+    reconcilableItem
+}: IReconcileButtonProps) => {
+    const [hasError, setHasError] = useState<boolean>(false);
+    const [errorText, setErrorText] = useState<string>("");
+    const [isReconciling, setIsReconciling] = useState<boolean>(false);
+    const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
 
+    useEffect(() => {
         const doFetch = async () => {
-          try {
-            await reconcilableItem.compliancyCheckerService.DoReconcileRequest(
-              reconcilableItem.reconcileUrl,
-            );
-            setHasError(false);
-            setIsDialogOpen(false);
-          } catch(e) {
-            setHasError(true);
-            setErrorText("Couldn't fulfill reconcile request.");
-          }
-          setIsReconciling(false)
+            try {
+                await reconcilableItem.compliancyCheckerService.DoReconcileRequest(
+                    reconcilableItem.reconcileUrl
+                );
+                setHasError(false);
+                setIsDialogOpen(false);
+            } catch (e) {
+                setHasError(true);
+                setErrorText("Couldn't fulfill reconcile request.");
+            }
+            setIsReconciling(false);
         };
 
-        useEffect(() => {
-          if(isReconciling) {
+        if (isReconciling) {
             doFetch();
-          }
-        }, [isReconciling]);
+        }
+    }, [
+        isReconciling,
+        reconcilableItem.compliancyCheckerService,
+        reconcilableItem.reconcileUrl
+    ]);
 
-        return (
-            <div>
-                <Button
-                    primary={true}
-                    iconProps = {{ iconName: "TriggerAuto" }}
-                    onClick={() => { setIsDialogOpen(true); }}
-                    text="Reconcile" disabled={(reconcilableItem.reconcileUrl === "")} />
-                  {isDialogOpen && (
-                      <Dialog
-                          titleProps={{ text: "Confirm reconciliation" }}
-                          footerButtonProps={[
-                              {
-                                  text: "Cancel",
-                                  onClick: () => {
-                                    setIsDialogOpen(false)
-                                    setHasError(false);
-                                  }
-                              },
-                              {
-                                  text: "Reconcile",
-                                  onClick: () => {
-                                    setIsReconciling(true);
-                                  },
-                                  primary: true,
-                                  disabled: isReconciling
-                              }
-                          ]}
-                          onDismiss={() => {
-                            setIsDialogOpen(false);
-                            setHasError(false)
-                          }}
-                      >
-                          {hasError  &&
-                            <MessageCard
-                              // @ts-ignore
-                              severity={MessageCardSeverity.Error}
-                              onDismiss={() => {
+    return (
+        <div>
+            <Button
+                primary={true}
+                iconProps={{ iconName: "TriggerAuto" }}
+                onClick={() => {
+                    setIsDialogOpen(true);
+                }}
+                text="Reconcile"
+                disabled={reconcilableItem.reconcileUrl === ""}
+            />
+            {isDialogOpen && (
+                <Dialog
+                    titleProps={{ text: "Confirm reconciliation" }}
+                    footerButtonProps={[
+                        {
+                            text: "Cancel",
+                            onClick: () => {
+                                setIsDialogOpen(false);
                                 setHasError(false);
-                              }}>
-                                {errorText}
-                            </MessageCard>}
-                          {isReconciling &&
-                            <Status
-                              {...Statuses.Running}
-                              key="reconciling"
-                              // @ts-ignore
-                              size={StatusSize.xl}
-                              text="Reconciling..."
-                            />}
-                          <p>Are you sure? Reconciling will make the following changes:</p>
-                          <SimpleList width={"100%"} itemProvider={
-                              new ArrayItemProvider<string>(reconcilableItem.reconcileImpact)}
-                          />
-                      </Dialog>)}
-            </div>
-        );
-    };
+                            }
+                        },
+                        {
+                            text: "Reconcile",
+                            onClick: () => {
+                                setIsReconciling(true);
+                            },
+                            primary: true,
+                            disabled: isReconciling
+                        }
+                    ]}
+                    onDismiss={() => {
+                        setIsDialogOpen(false);
+                        setHasError(false);
+                    }}
+                >
+                    {hasError && (
+                        <MessageCard
+                            // @ts-ignore
+                            severity={MessageCardSeverity.Error}
+                            onDismiss={() => {
+                                setHasError(false);
+                            }}
+                        >
+                            {errorText}
+                        </MessageCard>
+                    )}
+                    {isReconciling && (
+                        <Status
+                            {...Statuses.Running}
+                            key="reconciling"
+                            // @ts-ignore
+                            size={StatusSize.xl}
+                            text="Reconciling..."
+                        />
+                    )}
+                    <p>
+                        Are you sure? Reconciling will make the following
+                        changes:
+                    </p>
+                    <SimpleList
+                        width={"100%"}
+                        itemProvider={
+                            new ArrayItemProvider<string>(
+                                reconcilableItem.reconcileImpact
+                            )
+                        }
+                    />
+                </Dialog>
+            )}
+        </div>
+    );
+};
 
 export default ReconcileButton;
