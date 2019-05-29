cfb2324c017c9321b336fdae3d8198f80a2c16ee
Verhaar, CA (Kees) <kees.verhaar@rabobank.nl>
2019-05-29 10:05:42 +0200

    Update story to include correct syntax

--- src/Releases.stories.tsx
+++ src/Releases.stories.tsx
@@ -1,10 +1,5 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { CompliancyCheckerService } from "./services/CompliancyCheckerService";
-import { DummyAzDoService } from './services/DummyAzDoService';
 import Releases from "./Releases";
 
-storiesOf("Releases", module).add("default", () => (
-    <Releases azDoService={new DummyAzDoService()}
-    />
-));
+storiesOf("Releases", module).add("default", () => <Releases />);
