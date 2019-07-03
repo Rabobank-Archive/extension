61a87bce88314390c129d6be09a36c1bff6fe2fd
kleefd <2manwypn6swv2stt3ooaxjgx7nvda36j4cfg3htzxrutzbceew3a>
2019-07-03 16:04:03 +0200

    Added test for token which expires in the future 20

--- src/services/AzDoService.spec.tsx
+++ src/services/AzDoService.spec.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { IReleaseRule } from "./IAzDoService";
 import { IsTokenExpired } from "./AzDoService";
+import * as SDK from "azure-devops-extension-sdk";
 
 describe("AzDoService", () => {
     it("should return undefined if property not present in report", () => {
@@ -27,5 +28,12 @@ describe("AzDoService", () => {
             "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1laWQiOiIxNjIwNTQ0NC03ZjY1LTYyMzctODM4Yi00NzNmNjhhOWE3YjMiLCJ0aWQiOiI2ZTkzYTYyNi04YWNhLTRkYzEtOTE5MS1jZTI5MWI0Yjc1YTEiLCJpc3MiOiJhcHAudnNzcHMudmlzdWFsc3R1ZGlvLmNvbSIsImF1ZCI6Ijc4YzY4ZGIxLTIzMjMtNGE0OC04M2Y3LWY0MjA3MGY2ZTYzMiIsIm5iZiI6MTU2MjE1NTUzNSwiZXhwIjoxNTYyMTU5NzM1fQ.syVdJ-WC7JgpOmP9Gs-ULKyER58nNAEc_SdsCCHBNj4";
         var expired = IsTokenExpired(token);
         expect(expired).toBe(true);
+    });
+
+    //Token expires Monday 7 may 2035
+    it("should return false if token not expired", () => {
+        let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1laWQiOiIxNjIwNTQ0NC03ZjY1LTYyMzctODM4Yi00NzNmNjhhOWE3YjMiLCJ0aWQiOiI2ZTkzYTYyNi04YWNhLTRkYzEtOTE5MS1jZTI5MWI0Yjc1YTEiLCJpc3MiOiJhcHAudnNzcHMudmlzdWFsc3R1ZGlvLmNvbSIsImF1ZCI6Ijc4YzY4ZGIxLTIzMjMtNGE0OC04M2Y3LWY0MjA3MGY2ZTYzMiIsIm5iZiI6MTU2MjE1NTUzNSwiZXhwIjoyMDYyMTU5NzM1fQ.40-RT42P8lkKI2igtz2BahQoJO9OnnKC6I3VpCvWY5U";
+        var expired = IsTokenExpired(token);
+        expect(expired).toBe(false);
     });
 });
