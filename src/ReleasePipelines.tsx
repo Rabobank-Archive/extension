6c5efb9d6012069612fbc61b1476e62b05da5ff8
Manuel Riezebosch <mriezebosch@gmail.com>
2020-01-30 09:07:48 +0100

    refactor(release-pipelines): convert to function component

--- src/ReleasePipelines.tsx
+++ src/ReleasePipelines.tsx
@@ -1,13 +1,11 @@
 import * as React from "react";
-import {
-    IBuildPipelinesReport,
-    IItemReport,
-    IReleasePipelinesReport
-} from "./services/IAzDoService";
+import { IReleasePipelinesReport } from "./services/IAzDoService";
 import { Page } from "azure-devops-ui/Page";
 import { SurfaceBackground, Surface } from "azure-devops-ui/Surface";
+
 import MasterDetail from "./components/MasterDetail";
 import CompliancyHeader from "./components/CompliancyHeader";
+
 import "./css/styles.css";
 import { GetAzDoReportsFromDocumentStorage } from "./services/AzDoService";
 import { HasReconcilePermission } from "./services/CompliancyCheckerService";
@@ -19,113 +17,79 @@ import {
 import { withAITracking } from "@microsoft/applicationinsights-react-js";
 import ErrorBar from "./components/ErrorBar";
 import InfoBlock from "./components/InfoBlock";
+import { useState, useEffect, useReducer } from "react";
 
-interface IReleasePipelinesProps {}
+const ReleasePipelines = () => {
+    const [data, setData] = useState<IReleasePipelinesReport>({
+        date: new Date(),
+        hasReconcilePermissionUrl: "",
+        rescanUrl: "",
+        reports: []
+    });
+    const [loading, setLoading] = useState<boolean>(true);
+    const [reload, forceReload] = useReducer(x => x + 1, 0); // https://reactjs.org/docs/hooks-faq.html#is-there-something-like-forceupdate
+    const [hasReconcilePermission, setHasReconcilePermission] = useState<
+        boolean
+    >(false);
+    const [error, setError] = useState<string>("");
 
-interface IState {
-    isLoading: boolean;
-    isRescanning: boolean;
-    releasePipelinesReport: IBuildPipelinesReport;
-    hasReconcilePermission: boolean;
-    errorText: string;
-}
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const report = await GetAzDoReportsFromDocumentStorage<
+                    IReleasePipelinesReport
+                >("releasepipelines");
+                const hasReconcilePermission = await HasReconcilePermission(
+                    report.hasReconcilePermissionUrl
+                );
 
-class ReleasePipelines extends React.Component<IReleasePipelinesProps, IState> {
-    constructor(props: IReleasePipelinesProps) {
-        super(props);
-
-        this.state = {
-            releasePipelinesReport: {
-                date: new Date(),
-                hasReconcilePermissionUrl: "",
-                rescanUrl: "",
-                reports: []
-            },
-            isLoading: true,
-            isRescanning: false,
-            hasReconcilePermission: false,
-            errorText: ""
-        };
-    }
-
-    private async getData(): Promise<void> {
-        try {
-            const releasePipelinesReport = await GetAzDoReportsFromDocumentStorage<
-                IReleasePipelinesReport
-            >("releasepipelines");
-            const hasReconcilePermission = await HasReconcilePermission(
-                releasePipelinesReport.hasReconcilePermissionUrl
-            );
-
-            this.setState({
-                isLoading: false,
-                releasePipelinesReport: releasePipelinesReport,
-                hasReconcilePermission: hasReconcilePermission,
-                errorText: ""
-            });
-        } catch {
-            this.setState({
-                isLoading: false,
-                errorText:
+                setHasReconcilePermission(hasReconcilePermission);
+                setData(report);
+                setError("");
+            } catch {
+                setError(
                     "Something went wrong while retrieving report data. Please try again later, or contact TAS if the issue persists."
-            });
-        }
-    }
+                );
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, [reload]);
 
-    async componentDidMount() {
+    useEffect(() => {
         trackEvent("[Release Pipelines] Page opened");
         trackPageview();
-        await this.getData();
-    }
+    }, []);
 
-    render() {
-        return (
-            // @ts-ignore
-            <Surface background={SurfaceBackground.neutral}>
-                <Page className="flex-grow">
-                    <CompliancyHeader
-                        headerText="Release pipeline compliancy"
-                        lastScanDate={this.state.releasePipelinesReport.date}
-                        rescanUrl={this.state.releasePipelinesReport.rescanUrl}
-                        onRescanFinished={async () => {
-                            await this.getData();
-                        }}
-                    />
+    return (
+        // @ts-ignore
+        <Surface background={SurfaceBackground.neutral}>
+            <Page className="flex-grow">
+                <CompliancyHeader
+                    headerText="Release pipeline compliancy"
+                    lastScanDate={data.date}
+                    rescanUrl={data.rescanUrl}
+                    onRescanFinished={async () => {
+                        forceReload({});
+                    }}
+                />
 
-                    <ErrorBar
-                        message={this.state.errorText}
-                        onDismiss={() => this.setState({ errorText: "" })}
-                    />
-
-                    <InfoBlock showMoreInfoText={true} />
-
-                    <div className="page-content-top full-size">
-                        {this.state.isLoading ? (
-                            <div className="page-content">Loading...</div>
-                        ) : (
-                            this.getTabContent()
-                        )}
-                    </div>
-                </Page>
-            </Surface>
-        );
-    }
-
-    getTabContent(): React.ReactNode {
-        return (
-            <MasterDetail
-                title="Pipelines"
-                hasReconcilePermission={this.state.hasReconcilePermission}
-                data={this.state.releasePipelinesReport.reports.sort(
-                    compareItemReports
-                )}
-            />
-        );
-    }
-}
+                <ErrorBar message={error} onDismiss={() => setError("")} />
+                <InfoBlock showMoreInfoText={true} />
+                <div className="page-content-top full-size">
+                    {loading ? (
+                        <div className="page-content">Loading...</div>
+                    ) : (
+                        <MasterDetail
+                            title="Pipelines"
+                            hasReconcilePermission={hasReconcilePermission}
+                            data={data.reports}
+                        />
+                    )}
+                </div>
+            </Page>
+        </Surface>
+    );
+};
 
 export default withAITracking(appInsightsReactPlugin, ReleasePipelines);
-
-function compareItemReports(a: IItemReport, b: IItemReport) {
-    return a.item.localeCompare(b.item);
-}
