09d37d34496eb72a653ec7f6eb688940ca263359
Manuel Riezebosch <mriezebosch@gmail.com>
2020-01-30 08:24:23 +0100

    refactor(releases): convert to function component

--- src/Releases.tsx
+++ src/Releases.tsx
@@ -32,6 +32,7 @@ import ErrorBar from "./components/ErrorBar";
 import InfoBlock from "./components/InfoBlock";
 import { SurfaceBackground, Surface } from "azure-devops-ui/Surface";
 import { getDevopsUiStatus } from "./services/Status";
+import { useEffect, useState } from "react";
 
 interface ITableItem extends ISimpleTableCell {
     pipeline: string;
@@ -46,217 +47,195 @@ interface ITableItem extends ISimpleTableCell {
     sM9ChangeUrl: string;
 }
 
-interface IReleaseProps {}
+const Releases = () => {
+    const itemProvider = new ObservableArray<ITableItem>();
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string>("");
 
-class Releases extends React.Component<
-    IReleaseProps,
-    { report: IReleaseReport; isLoading: boolean; errorText: string }
-> {
-    private itemProvider = new ObservableArray<any>();
-
-    constructor(props: IReleaseProps) {
-        super(props);
-        this.state = {
-            report: {
-                reports: []
-            },
-            isLoading: true,
-            errorText: ""
-        };
-    }
-
-    async componentDidMount() {
+    useEffect(() => {
         trackEvent("[Releases] Page opened");
         trackPageview();
+    }, []);
 
-        try {
-            const report = await GetAzDoReportsFromDocumentStorage<
-                IReleaseReport
-            >("Releases");
-            this.itemProvider.push(
-                ...report.reports.map<ITableItem>(x => ({
-                    pipeline: x.pipeline,
-                    release: x.release,
-                    environment: x.environment,
-                    createdDate: x.createdDate,
-                    hasApprovalOptions: getDevopsUiStatus(x.hasApprovalOptions),
-                    hasBranchFilterForAllArtifacts: getDevopsUiStatus(
-                        x.hasBranchFilterForAllArtifacts
-                    ),
-                    usesManagedAgentsOnly: getDevopsUiStatus(
-                        x.usesManagedAgentsOnly
-                    ),
-                    allArtifactsAreFromBuild: getDevopsUiStatus(
-                        x.allArtifactsAreFromBuild
-                    ),
-                    sM9ChangeId: x.sM9ChangeId || "-",
-                    sM9ChangeUrl: x.sM9ChangeUrl || ""
-                }))
-            );
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const report = await GetAzDoReportsFromDocumentStorage<
+                    IReleaseReport
+                >("Releases");
+                itemProvider.push(
+                    ...report.reports.map<ITableItem>(x => ({
+                        pipeline: x.pipeline,
+                        release: x.release,
+                        environment: x.environment,
+                        createdDate: x.createdDate,
+                        hasApprovalOptions: getDevopsUiStatus(
+                            x.hasApprovalOptions
+                        ),
+                        hasBranchFilterForAllArtifacts: getDevopsUiStatus(
+                            x.hasBranchFilterForAllArtifacts
+                        ),
+                        usesManagedAgentsOnly: getDevopsUiStatus(
+                            x.usesManagedAgentsOnly
+                        ),
+                        allArtifactsAreFromBuild: getDevopsUiStatus(
+                            x.allArtifactsAreFromBuild
+                        ),
+                        sM9ChangeId: x.sM9ChangeId || "-",
+                        sM9ChangeUrl: x.sM9ChangeUrl || ""
+                    }))
+                );
 
-            this.setState({ isLoading: false, report: report, errorText: "" });
-        } catch (e) {
-            if (e.status === 404) {
-                this.setState({
-                    isLoading: false
-                });
-            } else {
-                this.setState({
-                    isLoading: false,
-                    errorText:
+                setError("");
+            } catch (e) {
+                if (e.status !== 404) {
+                    setError(
                         "Something went wrong while retrieving report data. Please try again later, or contact TAS if the issue persists."
-                });
+                    );
+                }
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, [itemProvider]);
+
+    const columns: ITableColumn<ITableItem>[] = [
+        {
+            id: "pipeline",
+            name: "Pipeline",
+            onSize: onSize,
+            renderCell: renderSimpleCell,
+            width: -3,
+            sortProps: {
+                ariaLabelAscending: "Sorted A to Z",
+                ariaLabelDescending: "Sorted Z to A"
+            }
+        },
+        {
+            id: "release",
+            name: "Release",
+            onSize: onSize,
+            renderCell: renderSimpleCell,
+            width: -2,
+            sortProps: {
+                ariaLabelAscending: "Sorted A to Z",
+                ariaLabelDescending: "Sorted Z to A"
+            }
+        },
+        {
+            id: "environment",
+            name: "Environment",
+            onSize: onSize,
+            renderCell: renderSimpleCell,
+            width: -2,
+            sortProps: {
+                ariaLabelAscending: "Sorted A to Z",
+                ariaLabelDescending: "Sorted Z to A"
+            }
+        },
+        {
+            id: "createdDate",
+            name: "Created",
+            onSize: onSize,
+            width: -2,
+            renderCell: renderDate,
+            sortProps: {
+                ariaLabelAscending: "Sorted Oldest to Newest",
+                ariaLabelDescending: "Sorted Newest to Oldest"
+            }
+        },
+        {
+            id: "hasApprovalOptions",
+            name: "Approved",
+            onSize: onSize,
+            renderCell: renderCheckmark,
+            className: "center",
+            width: new ObservableValue(85),
+            sortProps: {
+                ariaLabelAscending: "Sorted A to Z",
+                ariaLabelDescending: "Sorted Z to A"
+            }
+        },
+        {
+            id: "hasBranchFilterForAllArtifacts",
+            name: "Has branch filters",
+            onSize: onSize,
+            renderCell: renderCheckmark,
+            className: "center",
+            width: new ObservableValue(130),
+            sortProps: {
+                ariaLabelAscending: "Sorted A to Z",
+                ariaLabelDescending: "Sorted Z to A"
+            }
+        },
+        {
+            id: "usesManagedAgentsOnly",
+            name: "Uses managed agents",
+            onSize: onSize,
+            renderCell: renderCheckmark,
+            className: "center",
+            width: new ObservableValue(160),
+            sortProps: {
+                ariaLabelAscending: "Sorted A to Z",
+                ariaLabelDescending: "Sorted Z to A"
+            }
+        },
+        {
+            id: "allArtifactsAreFromBuild",
+            name: "Artifacts are from build",
+            onSize: onSize,
+            renderCell: renderCheckmark,
+            className: "center",
+            width: new ObservableValue(172),
+            sortProps: {
+                ariaLabelAscending: "Sorted A to Z",
+                ariaLabelDescending: "Sorted Z to A"
+            }
+        },
+        {
+            id: "sM9ChangeId",
+            name: "SM9 Change",
+            onSize: onSize,
+            renderCell: renderLink,
+            width: new ObservableValue(130),
+            sortProps: {
+                ariaLabelAscending: "Sorted A to Z",
+                ariaLabelDescending: "Sorted Z to A"
             }
         }
-    }
+    ];
 
-    render() {
-        const columns: ITableColumn<ITableItem>[] = [
-            {
-                id: "pipeline",
-                name: "Pipeline",
-                onSize: onSize,
-                renderCell: renderSimpleCell,
-                width: -3,
-                sortProps: {
-                    ariaLabelAscending: "Sorted A to Z",
-                    ariaLabelDescending: "Sorted Z to A"
-                }
-            },
-            {
-                id: "release",
-                name: "Release",
-                onSize: onSize,
-                renderCell: renderSimpleCell,
-                width: -2,
-                sortProps: {
-                    ariaLabelAscending: "Sorted A to Z",
-                    ariaLabelDescending: "Sorted Z to A"
-                }
-            },
-            {
-                id: "environment",
-                name: "Environment",
-                onSize: onSize,
-                renderCell: renderSimpleCell,
-                width: -2,
-                sortProps: {
-                    ariaLabelAscending: "Sorted A to Z",
-                    ariaLabelDescending: "Sorted Z to A"
-                }
-            },
-            {
-                id: "createdDate",
-                name: "Created",
-                onSize: onSize,
-                width: -2,
-                renderCell: renderDate,
-                sortProps: {
-                    ariaLabelAscending: "Sorted Oldest to Newest",
-                    ariaLabelDescending: "Sorted Newest to Oldest"
-                }
-            },
-            {
-                id: "hasApprovalOptions",
-                name: "Approved",
-                onSize: onSize,
-                renderCell: renderCheckmark,
-                className: "center",
-                width: new ObservableValue(85),
-                sortProps: {
-                    ariaLabelAscending: "Sorted A to Z",
-                    ariaLabelDescending: "Sorted Z to A"
-                }
-            },
-            {
-                id: "hasBranchFilterForAllArtifacts",
-                name: "Has branch filters",
-                onSize: onSize,
-                renderCell: renderCheckmark,
-                className: "center",
-                width: new ObservableValue(130),
-                sortProps: {
-                    ariaLabelAscending: "Sorted A to Z",
-                    ariaLabelDescending: "Sorted Z to A"
-                }
-            },
-            {
-                id: "usesManagedAgentsOnly",
-                name: "Uses managed agents",
-                onSize: onSize,
-                renderCell: renderCheckmark,
-                className: "center",
-                width: new ObservableValue(160),
-                sortProps: {
-                    ariaLabelAscending: "Sorted A to Z",
-                    ariaLabelDescending: "Sorted Z to A"
-                }
-            },
-            {
-                id: "allArtifactsAreFromBuild",
-                name: "Artifacts are from build",
-                onSize: onSize,
-                renderCell: renderCheckmark,
-                className: "center",
-                width: new ObservableValue(172),
-                sortProps: {
-                    ariaLabelAscending: "Sorted A to Z",
-                    ariaLabelDescending: "Sorted Z to A"
-                }
-            },
-            {
-                id: "sM9ChangeId",
-                name: "SM9 Change",
-                onSize: onSize,
-                renderCell: renderLink,
-                width: new ObservableValue(130),
-                sortProps: {
-                    ariaLabelAscending: "Sorted A to Z",
-                    ariaLabelDescending: "Sorted Z to A"
-                }
-            }
-        ];
+    return (
+        // @ts-ignore
+        <Surface background={SurfaceBackground.neutral}>
+            <Page className="flex-grow">
+                <Header
+                    title={"Release compliancy"}
+                    // @ts-ignore
+                    titleSize={TitleSize.Large}
+                    titleIconProps={{ iconName: "OpenSource" }}
+                />
 
-        return (
-            // @ts-ignore
-            <Surface background={SurfaceBackground.neutral}>
-                <Page className="flex-grow">
-                    <Header
-                        title={"Release compliancy"}
-                        // @ts-ignore
-                        titleSize={TitleSize.Large}
-                        titleIconProps={{ iconName: "OpenSource" }}
-                    />
-
-                    <ErrorBar
-                        message={this.state.errorText}
-                        onDismiss={() => this.setState({ errorText: "" })}
-                    />
-
-                    <InfoBlock showMoreInfoText={false} />
-
-                    <div className="page-content page-content-top">
-                        <Card>
-                            {this.state.isLoading ? (
-                                <div className="page-content">Loading...</div>
-                            ) : (
-                                <Table<ITableItem>
-                                    columns={columns}
-                                    itemProvider={this.itemProvider}
-                                    behaviors={[
-                                        sortingBehavior(
-                                            this.itemProvider,
-                                            columns
-                                        )
-                                    ]}
-                                />
-                            )}
-                        </Card>
-                    </div>
-                </Page>
-            </Surface>
-        );
-    }
-}
+                <ErrorBar message={error} onDismiss={() => setError("")} />
+                <InfoBlock showMoreInfoText={false} />
+                <div className="page-content page-content-top">
+                    <Card>
+                        {loading ? (
+                            <div className="page-content">Loading...</div>
+                        ) : (
+                            <Table<ITableItem>
+                                columns={columns}
+                                itemProvider={itemProvider}
+                                behaviors={[
+                                    sortingBehavior(itemProvider, columns)
+                                ]}
+                            />
+                        )}
+                    </Card>
+                </div>
+            </Page>
+        </Surface>
+    );
+};
 
 export default withAITracking(appInsightsReactPlugin, Releases);
