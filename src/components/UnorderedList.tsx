361e574171faded2a0a8a8a128494076359b8be0
Manuel Riezebosch <mriezebosch@gmail.com>
2020-02-03 08:49:39 +0100

    refactor(UnorderedList): convert to function component

--- src/components/UnorderedList.tsx
+++ src/components/UnorderedList.tsx
@@ -1,36 +1,19 @@
 import React from "react";
-import { List, IListItemDetails } from "azure-devops-ui/List";
+import "./UnorderedList.css";
+import { List } from "azure-devops-ui/List";
 import { IItemProvider } from "azure-devops-ui/Utilities/Provider";
-import { IReadonlyObservableValue } from "azure-devops-ui/Core/Observable";
-import "../css/UnorderedList.css";
 
-export class UnorderedList extends React.Component<
-    {
-        itemProvider: IItemProvider<
-            string | IReadonlyObservableValue<string | undefined>
-        >;
-    },
-    {}
-> {
-    private renderItemRow(
-        rowIndex: number,
-        item: string,
-        details: IListItemDetails<string>
-    ): JSX.Element {
-        return (
-            <tr className="bolt-list-row" data-row-index={rowIndex}>
-                <td className="unordered-list-cell">&bull;</td>
-                <td className="unordered-list-cell">{item}</td>
-            </tr>
-        );
-    }
+export const UnorderedList = ({
+    itemProvider
+}: {
+    itemProvider: IItemProvider<string>;
+}) => {
+    const renderRow = (rowIndex: number, item: string): JSX.Element => (
+        <tr className="bolt-list-row" data-row-index={rowIndex}>
+            <td className="unordered-list-cell">&bull;</td>
+            <td className="unordered-list-cell">{item}</td>
+        </tr>
+    );
 
-    render() {
-        return (
-            <List
-                itemProvider={this.props.itemProvider}
-                renderRow={this.renderItemRow}
-            />
-        );
-    }
-}
+    return <List itemProvider={itemProvider} renderRow={renderRow} />;
+};
